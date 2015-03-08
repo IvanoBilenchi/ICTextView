e21479ef0361a31dc1f58212daf7ca479e0b9f22
Ivano Bilenchi <exile@live.it>
2015-03-08 13:14:21 +0100

    Updated copyright year

--- ICTextView/ICRangeUtils.m
+++ ICTextView/ICRangeUtils.m
@@ -11,7 +11,7 @@
  *
  * License:
  * --------
- * Copyright (c) 2013-2014 Ivano Bilenchi
+ * Copyright (c) 2013-2015 Ivano Bilenchi
  *
  * Permission is hereby granted, free of charge, to any person
  * obtaining a copy of this software and associated documentation
--- ICTextView/ICRegularExpression.h
+++ ICTextView/ICRegularExpression.h
@@ -21,7 +21,7 @@
  *
  * License:
  * --------
- * Copyright (c) 2013-2014 Ivano Bilenchi
+ * Copyright (c) 2013-2015 Ivano Bilenchi
  *
  * Permission is hereby granted, free of charge, to any person
  * obtaining a copy of this software and associated documentation
